3061116c60cb299bf6c329b050fe0d5801bbc999
Gooner9803 <vache.khachatryan1998@gmail.com>
2022-03-01 22:01:09 +0400

    __

---
+++ src/main/java/com/epam/backendtask01/BackEndTask01Application.java
@@ -0,0 +1,13 @@
+package com.epam.backendtask01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndTask01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndTask01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/epam/backendtask01/BackEndTask01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.backendtask01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndTask01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
